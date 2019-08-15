284dbc5a067981465102e40444c6a481301aa5e3
Daniel Leal <danielleal94@gmail.com>
2019-08-15 17:14:24 -0300

    init repo

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import 'express-async-errors';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: false }));
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App();
