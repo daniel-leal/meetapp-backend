359f517e67320836b368523a79514cad0506c6a1
Daniel Leal <danielleal94@gmail.com>
2019-08-19 19:53:15 -0300

    add user

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from 'express';
 import 'express-async-errors';
 import routes from './routes';
 
+import './database';
+
 class App {
   constructor() {
     this.server = express();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 
+// Controllers
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.send('Hello World!'));
+routes.get('/', (req, res) =>
+  res.json({ status: 'ok', version: 1.0, app: 'MeetaApp!' })
+);
+
+routes.post('/users', UserController.store);
 
 export default routes;
