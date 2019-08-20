384c7586ac69825d228c358a2f9ac3d5615d79a7
Daniel Leal <danielleal94@gmail.com>
2019-08-19 21:54:43 -0300

    autenticacao e atualizacao do usuario

---
+++ src/app/validators/UserUpdate.js
@@ -0,0 +1,25 @@
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string()
+        .min(6)
+        .required(),
+      password: Yup.string().min(6),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: 'Validation Fails', messages: err.inner });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,13 @@ import { Router } from 'express';
 
 // Controllers
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+// Validators
+import validateUserStore from './app/validators/UserStore';
+import validateUserUpdate from './app/validators/UserUpdate';
 
 const routes = new Router();
 
@@ -9,6 +16,17 @@ routes.get('/', (req, res) =>
   res.json({ status: 'ok', version: 1.0, app: 'MeetaApp!' })
 );
 
-routes.post('/users', UserController.store);
+/**
+ * Auth
+ */
+routes.post('/users', validateUserStore, UserController.store);
+routes.post('/sessions', SessionController.store);
+
+/**
+ * Logged Users
+ */
+routes.use(authMiddleware);
+
+routes.put('/users', validateUserUpdate, UserController.update);
 
 export default routes;
