a9023a0461415f708e073c3ca37c0be1bd551f9f
Daniel Leal <danielleal94@gmail.com>
2019-08-21 20:21:16 -0300

    Queue mails

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -3,7 +3,8 @@ import { Op } from 'sequelize';
 import Subscription from '../models/Subscription';
 import User from '../models/User';
 import Meetup from '../models/Meetup';
-import Mail from '../../lib/Mail';
+import SubscriptionMail from '../jobs/SubscriptionMail';
+import Queue from '../../lib/Queue';
 
 class SubscriptionController {
   async index(req, res) {
@@ -70,21 +71,9 @@ class SubscriptionController {
       meetup_id: meetup.id,
     });
 
-    // await Queue.add(SubscriptionMail.key, {
-    //   meetup,
-    //   user,
-    // });
-
-    await Mail.sendMail({
-      to: `${meetup.User.name} <${meetup.User.email}>`,
-      subject: `[${meetup.title}] Nova inscrição`,
-      template: 'subscription',
-      context: {
-        organizer: meetup.User.name,
-        meetup: meetup.title,
-        user: user.name,
-        email: user.email,
-      },
+    await Queue.add(SubscriptionMail.key, {
+      meetup,
+      user,
     });
 
     return res.json(subscription);
